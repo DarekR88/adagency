e9bfd77dca92dc055b4a09930eea669e4752476b
Darek Radke <dradmclad@gmail.com>
2023-04-19 21:38:29 -0500

    text-img component finished and content added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import "./Home.scss";
 import { Link } from "react-router-dom";
+import TextImg from "../TextImg/TextImg";
 import meetingImg from "../../static/images/meeting.jpg";
 import socialImg from "../../static/images/socialMedia.jpg";
 import websiteImg from "../../static/images/websiteDark.jpg";
@@ -12,7 +13,7 @@ const Home = () => {
     <div className="home" id="home">
       <div className="home-banner">
         <img className="home-img" src={meetingImg} alt="meeting image" />
-        <h1 className="home-title">Great Ad</h1>
+        <h1 className="home-title">AdCo</h1>
         <h2 className="img-blurb">
           Complete marketing solutions for the social media world
         </h2>
@@ -26,40 +27,35 @@ const Home = () => {
           </p>
           <div className="line-bottom"></div>
         </div>
-        <div className="home-offerings">
-          <img className="offerings-img" src={websiteImg} />
-          <h2 className="offerings-title">Websites</h2>
-          <p className="blurb">Custom websites for the modern internet.</p>
-          <p className="cost">
-            Custom websites starting at $500.<span className="cents">00</span>
-          </p>
-        </div>
-        <div className="home-offerings">
-          <img className="offerings-img" src={socialImg} />
-          <h2 className="offerings-title">Social Media Campaign</h2>
-          <p className="blurb">Custom posts across all social media.</p>
-          <p className="cost">
-            Social media plans starting at $500.
-            <span className="cents">00</span>
-          </p>
-        </div>
-        <div className="home-offerings">
-          <img className="offerings-img" src={brandImg} />
-          <h2 className="offerings-title">Branding</h2>
-          <p className="blurb">Up your brand recognition.</p>
-          <p className="cost">
-            Custom branding starting at $500.
-            <span className="cents">00</span>
-          </p>
-        </div>
-        <div className="home-offerings">
-          <img className="offerings-img" src={paperImg} />
-          <h2 className="offerings-title">Paper</h2>
-          <p className="blurb">Paper ad campaign based on your branding.</p>
-          <p className="cost">
-            Paper campaigns starting at $500.
-            <span className="cents">00</span>
-          </p>
+        <div className="services">
+          <TextImg
+            background="black"
+            imgSrc={websiteImg}
+            color="white"
+            title="Websites"
+            text="Custom websites for the modern internet"
+          />
+          <TextImg
+            background="white"
+            imgSrc={socialImg}
+            color="black"
+            title="Social Media Campaign"
+            text="Custom posts across all social media"
+          />
+          <TextImg
+            background="black"
+            imgSrc={brandImg}
+            color="white"
+            title="Branding"
+            text="Up your brand recognition"
+          />
+          <TextImg
+            background="white"
+            imgSrc={paperImg}
+            color="black"
+            title="Paper"
+            text="Paper campaign based on your branding"
+          />
         </div>
       </div>
     </div>
